82fb763fa94fb8e2a4c9109cc6e0bf2065a179c2
bulkassi <158156178+bulkassi@users.noreply.github.com>
2025-07-29 21:38:20 +0300

    Add functions for creating and deleting the grid in container
    The amount of created grid items (squares) in container is based on inputed number of squares per side

--- script.js
+++ script.js
@@ -5,4 +5,28 @@ function hoveredOver(e) {
     e.target.classList.add("visited");
 }
 
+function deleteGrid() {
+  while (gridContainer.firstChild) {
+    gridContainer.removeChild(gridContainer.firstChild);
+  }
+}
+
+function createGrid(squaresPerSide) {
+  const gridItemHeight = 100 / squaresPerSide;
+  const gridItemWidth = 100 / squaresPerSide;
+
+  const gridItem = document.createElement("div");
+  gridItem.style.height = gridItemHeight + "%";
+  gridItem.style.width = gridItemWidth + "%";
+  gridItem.classList.add("grid-item");
+
+  const gridItemsAmount = squaresPerSide ** 2;
+  console.log(gridItemsAmount);
+  for (let i = 0; i < gridItemsAmount; i++) {
+    gridContainer.appendChild(gridItem.cloneNode());
+  }
+}
+
 gridContainer.addEventListener("mousemove", hoveredOver);
+deleteGrid();
+createGrid(10);
