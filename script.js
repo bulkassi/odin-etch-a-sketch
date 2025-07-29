b88eae6f0f1f1f6376ae57e23aee753ca33740fc
bulkassi <158156178+bulkassi@users.noreply.github.com>
2025-07-29 20:58:51 +0300

    Add the hovering over logic
    The container content turns grey when mouse is moved over it

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 const gridContainer = document.querySelector("#grid-container");
+
+function hoveredOver(e) {
+  e.target.classList.add("visited");
+}
+
+gridContainer.addEventListener("mousemove", hoveredOver);
